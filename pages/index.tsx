862308181805e35d4dd1d99a94492d929a6ca33a
pravinmobilefirst9 <pravin.mobilefirst@gmail.com>
2023-03-22 21:58:26 +0530

    Demo Testing App
    Demo Testing App

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Testing Page Pravin 2023&nbsp;
+            Demo Test App&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
