9181604982c6a1888d28f6b84d5bf13b1507226a
pravinmobilefirst9 <pravin.mobilefirst@gmail.com>
2023-03-23 07:53:16 +0530

    remove externals
    remove externals

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  externals: { 'agora-electron-sdk': 'commonjs2 agora-electron-sdk' }
+  reactStrictMode: true 
 }
 
 module.exports = nextConfig
