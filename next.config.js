a63aaf6965062d4d268e52213d9c74543357238d
pravinmobilefirst9 <pravin.mobilefirst@gmail.com>
2023-03-23 07:50:19 +0530

    Updated the changes on agora-electron-sdk
    Updated the changes on agora-electron-sdk

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  externals: { 'agora-electron-sdk': 'commonjs2 agora-electron-sdk' }
 }
 
 module.exports = nextConfig
